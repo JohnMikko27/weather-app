3729450f46cd9832fe6ceb59f78a7dc0f2533538
Mikko <realmiks27@gmail.com>
2023-09-09 08:01:07 -0700

    Add form to search for city

--- src/components/api.js
+++ src/components/api.js
@@ -7,23 +7,39 @@
  */
 
 const fetchData = async() => {
-    const input = document.querySelector('input');
-    const apiKey = '90ba82aa379048c3b86181742232108';
+  const input = document.querySelector('input');
+  const apiKey = '90ba82aa379048c3b86181742232108';
 
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${input.value}`, {mode: 'cors'});
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
-    // return data 
-}
+  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${input.value}`, {mode: 'cors'});
+  console.log(response);
+  // eslint-disable-next-line no-use-before-define
+  processData(response);
+  // return data 
+};
 
+const processData = async(response) => {
+  const data = await response.json();
+  const dataInfo = data.current;
+  
+  // object destructuring requires correct names
+  // eslint-disable-next-line camelcase
+  const {feelslike_c,feelslike_f} = dataInfo; 
+  console.log(data);
+  console.log(dataInfo);
+  console.log(feelslike_c, feelslike_f);
+};
 /* 
  * might have to move this event listener in controller or somthing
  * and have the callback be a displayWeather function or something 
  * where displayWeather will get data from the fetchData or whatever functions, create the appropriate ui, and display it
- *  
  */ 
 export default function s() {
-    const button = document.querySelector('button');
-    button.addEventListener('click', fetchData);
+//   const searchCityButton = document.querySelector('#searchCityButton');
+//   searchCityButton.addEventListener('click', fetchData);
+  const searchCityForm = document.querySelector('#searchCityForm');
+  searchCityForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetchData();
+    searchCityForm.reset();
+  });
 }
