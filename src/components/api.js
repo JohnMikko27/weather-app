fc937240555ec101c49ff44338e690737ad3de26
Mikko <realmiks27@gmail.com>
2023-09-09 07:19:43 -0700

    Create function that fetches data from api

--- src/components/api.js
+++ src/components/api.js
@@ -1,18 +1,29 @@
 /* 
- * when the client searches something up in the search bar and enters, 
- * then get that data (city) get that city's weather data from the api
+ * when the client searches something up in the search bar and enters, (controller.js)
+ * then get that data (city) get that city's weather data from the api (api.js)
+ * then display the information from that request onto the page (ui..js)
+ * remember to look at programming notes and apply them, especially OOP principles/DRY/SOLID principles
+ * make sure to utilize functions and api.js and ui.js should not have any functions within one another, it should only be within controller.js
  */
 
-// weather api key: 90ba82aa379048c3b86181742232108
-const apiKey = '90ba82aa379048c3b86181742232108'
-const city = document.querySelector('input');
-const button = document.querySelector('button');
+const fetchData = async() => {
+    const input = document.querySelector('input');
+    const apiKey = '90ba82aa379048c3b86181742232108';
+
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${input.value}`, {mode: 'cors'});
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    // return data 
+}
+
+/* 
+ * might have to move this event listener in controller or somthing
+ * and have the callback be a displayWeather function or something 
+ * where displayWeather will get data from the fetchData or whatever functions, create the appropriate ui, and display it
+ *  
+ */ 
 export default function s() {
-    button.addEventListener('click', async() => {
-        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city.value}`, {mode: 'cors'})
-        console.log(response)
-        const data = await response.json();
-        console.log(data);
-        
-    })
+    const button = document.querySelector('button');
+    button.addEventListener('click', fetchData);
 }
