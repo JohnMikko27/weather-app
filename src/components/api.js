3f864cacbda196fd5cb45191cb18e42e888d421b
Mikko <realmiks27@gmail.com>
2023-09-11 10:29:32 -0700

    Processed data from fetch request

--- src/components/api.js
+++ src/components/api.js
@@ -8,36 +8,33 @@
 
 
 
-// USE ANOTHER BRANCH
 
 
+const processData = async(response) => {
+  const data = await response.json();
+  const dataCurrent = data.current;
+  const dataLocation = data.location;
+    
+  // object destructuring requires exact names
+  // eslint-disable-next-line camelcase
+  const { condition, humidity, temp_c, temp_f, wind_mph } = dataCurrent; 
+  const { country, name, region } = dataLocation;
+  const weatherCondition = condition.text;
+
+  // eslint-disable-next-line camelcase
+  return { country, name, region, weatherCondition, humidity, temp_c, temp_f, wind_mph };
+};
 
 const fetchData = async() => {
   const input = document.querySelector('input');
   const apiKey = '90ba82aa379048c3b86181742232108';
 
   const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${input.value}`, {mode: 'cors'});
-  console.log(response);
-  // eslint-disable-next-line no-use-before-define
-  processData(response);
-  // return data 
+  const processedData = await processData(response);
+  console.log(processedData);
+  return processedData;
 };
 
-const processData = async(response) => {
-  const data = await response.json();
-  const dataCurrent = data.current;
-  const dataLocation = data.location;
-  
-  // object destructuring requires exact names
-  // eslint-disable-next-line camelcase
-  const { condition, humidity, temp_c, temp_f, wind_mph } = dataCurrent; 
-  const { country, name, region } = dataLocation;
-  console.log(data);
-  console.log(dataCurrent);
-  console.log(dataLocation);
-  console.log(condition.text, humidity, temp_c, temp_f, wind_mph);
-  console.log(country, name, region);
-};
 /* 
  * might have to move this event listener in controller or somthing
  * and have the callback be a displayWeather function or something 
